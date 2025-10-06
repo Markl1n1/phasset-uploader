ebd73c89c54287e2617f669875dfe537bc21badd
MarkL1N <151952963+Markl1n1@users.noreply.github.com>
2025-10-06 12:18:12 +0300

    =

---
+++ en/upload.php
@@ -0,0 +1,42 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = htmlspecialchars($_POST['name']);
+    $surname = htmlspecialchars($_POST['surname']);
+    $uploadDir = '../upload/en/' . date('Ymd') . '/';
+
+    if (!is_dir($uploadDir)) {
+        mkdir($uploadDir, 0777, true);
+    }
+
+    $uploadSuccess = true;
+    foreach ($_FILES['files']['tmp_name'] as $key => $tmp_name) {
+        $originalFileName = $_FILES['files']['name'][$key];
+        $fileExtension = pathinfo($originalFileName, PATHINFO_EXTENSION);
+        $fileBaseName = pathinfo($originalFileName, PATHINFO_FILENAME);
+        
+        $counter = 1;
+        $newFileName = "{$name}-{$surname}-{$counter}-{$fileBaseName}.{$fileExtension}";
+        $uploadFile = $uploadDir . $newFileName;
+
+        // Check for existing file and increment counter
+        while (file_exists($uploadFile)) {
+            $counter++;
+            $newFileName = "{$name}-{$surname}-{$counter}-{$fileBaseName}.{$fileExtension}";
+            $uploadFile = $uploadDir . $newFileName;
+        }
+
+        // Save the file
+        if (!move_uploaded_file($tmp_name, $uploadFile)) {
+            $uploadSuccess = false;
+            break;
+        }
+    }
+
+    if ($uploadSuccess) {
+        header("Location: index.php?status=success");
+    } else {
+        header("Location: index.php?status=error");
+    }
+    exit();
+}
+?>
---
+++ ru/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Philippos Asset Management | Прикрепите файлы</title>
+    <link rel="icon" type="image/png" sizes="32x32" href="../phasset.svg">
+    <link rel="icon" type="image/png" sizes="16x16" href="../phasset.svg">
+	<link rel="stylesheet" href="/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <div class="upload-container">
+      <img src="../phasset.svg" alt="Philippos Asset Management logo">
+      <h2>Загрузите подтверждение платежа / Фото документов</h2>
+      <form action="upload.php" method="post" enctype="multipart/form-data">
+        <input type="text" name="name" style="margin-bottom: 15px;" placeholder="Имя" required>
+        <input type="text" name="surname" placeholder="Фамилия" required>
+        <input type="file" name="files[]" id="file-upload" class="custom-file-input" multiple required>
+        <label for="file-upload" class="custom-file-label">
+          <img width="60" height="60" src="https://img.icons8.com/ios-filled/50/add-file.png" alt="add-file"/>
+          <span id="file-label-text">Прикрепите файлы тут</span>
+        </label>
+        <br>
+        <p style="color: #171620; font-style: italic;">Как правильно сделать фото? смотреть <a href="../template.png" target="_blank" style="color:#171620; text-decoration-line: underline;">шаблон</a>
+        </p>
+        <br>
+        <button type="submit">Загрузить</button>
+      </form>
+      <div class="message"> <?php
+            if (isset($_GET['status'])) {
+                if ($_GET['status'] == 'success') {
+                    echo '
+																		<p class="success">Файлы успешно загружены.</p>';
+                } elseif ($_GET['status'] == 'error') {
+                    echo '
+																		<p class="error">Попробуйте позже...</p>';
+                }
+            }
+            ?> </div>
+    </div>
+    <script>
+      document.getElementById('file-upload').addEventListener('change', function() {
+        var fileName = this.files.length > 0 ? this.files.length + ' file(s) selected' : 'Drag or drop file here';
+        document.getElementById('file-label-text').textContent = fileName;
+      });
+    </script>
+  </body>
+</html>
